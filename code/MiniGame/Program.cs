9122e3d98e2b3a993cefbafb3fe98b34b0eca7c3
LeonidMickhailov-tcore <Leonid.Mickhailov@Transcore.com>
2025-06-09 09:25:21 +0000

    Добавить игру "Камень, Ножницы, Бумага" с логикой выбора и повторной игры

--- code/MiniGame/Program.cs
+++ code/MiniGame/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Ready to build a mini game with .NET/C# with help from GitHub Copilot?");
+﻿using System;
+using MiniGame;
+
+class Program
+{
+    static void Main()
+    {
+        bool playAgain = true;
+        while (playAgain)
+        {
+            Console.WriteLine("Выберите предмет:");
+            foreach (var thing in Enum.GetValues(typeof(Things)))
+            {
+                Console.WriteLine($"{(int)thing} - {thing}");
+            }
+
+            Console.Write("Ваш выбор: ");
+            if (!int.TryParse(Console.ReadLine(), out int userChoice) ||
+                !Enum.IsDefined(typeof(Things), userChoice))
+            {
+                Console.WriteLine("Некорректный выбор.");
+                continue;
+            }
+
+            Things userThing = (Things)userChoice;
+
+            Random rnd = new Random();
+            Things computerThing = (Things)rnd.Next(0, 3);
+
+            Console.WriteLine($"Компьютер выбрал: {computerThing}");
+
+            if (userThing == computerThing)
+            {
+                Console.WriteLine("Ничья!");
+            }
+            else if (
+                (userThing == Things.Rock && computerThing == Things.Scissors) ||
+                (userThing == Things.Scissors && computerThing == Things.Paper) ||
+                (userThing == Things.Paper && computerThing == Things.Rock)
+            )
+            {
+                Console.WriteLine("Вы победили!");
+            }
+            else
+            {
+                Console.WriteLine("Победил компьютер!");
+            }
+
+            Console.Write("Сыграть ещё раз? (д/н): ");
+            var answer = Console.ReadLine();
+            playAgain = answer != null && answer.Trim().ToLower() == "д";
+            Console.WriteLine();
+        }
+        Console.WriteLine("Спасибо за игру!");
+    }
+}
+
